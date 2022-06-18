88ff6b38d2397ad5a0be971c8170fa0dc1f2767a
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-06-18 05:34:16 +0000

    Add database settings

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagram_rds_dev",
+    "password": "iamthedarkbotBORNin1999",
+    "database": "udagram_rds_dev",
+    "host": "udagram-rds-dev-db.cvyae3o2xiyx.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-bucket-gr1999-dev"
   },
   "jwt": {
     "secret": " "
